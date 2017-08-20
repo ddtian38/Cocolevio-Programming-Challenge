b9a51d02f5b6b0e0e72eed2f3b611f76f38de7fa
DESKTOP-VV8S0T5\danie <danieltian31@yahoo.com>
2017-08-19 23:51:17 -0500

    adjusted code after running tests

--- Cocolevio Programming Challenge.py
+++ Cocolevio Programming Challenge.py
@@ -1,6 +1,6 @@
 #Daniel Tian
 #Cocolevio Programming Challenge
-x = 25 #total material given for supply
+x = 35 #total material given for supply
 company = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'] #companies
 amount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #corresponding quantity of the material that companies request
 price = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30] #corresponding price that company is willing to pay
@@ -15,7 +15,7 @@ for i in index:
     n1 = float(price[i])
     n2 = float(amount[i])
     parent_rate.append(round(n1/n2,2))
-
+#Algorithm finds the highest rate from the list of companies and calculates the profit after selling material to the company with the highest rate. 
 while x > 0 and len(amount)!=0 and x > min(amount):
     amount_used = []
     soldcompany = []
@@ -23,17 +23,18 @@ while x > 0 and len(amount)!=0 and x > min(amount):
     index = []
     max_rate = max(parent_rate)
     ind_max_rate = [i for i, j in enumerate(parent_rate) if j == max_rate] #Retrieves corresponding index of the maximum price
-
+    
     for i in ind_max_rate:
-        a = amount[i]
-        p = price[i]
-        profit = profit + max_rate*a
-        #Subtracts amount sold from supply
-        x = x-a
-        
-        soldcompany.append(company[i])
-        amount_used.append(amount[i])
-        company_sold_to.append(company[i])
+        if x-amount[i] >= 0:
+            a = amount[i]
+            p = price[i]
+            profit = profit + max_rate*a
+            #Subtracts amount sold from supply
+            x = x-a
+            print(x)
+            soldcompany.append(company[i])
+            amount_used.append(amount[i])
+            company_sold_to.append(company[i])
         
     #Removes used highest rate 
     for r in parent_rate:
@@ -51,7 +52,7 @@ while x > 0 and len(amount)!=0 and x > min(amount):
         for b in company:
             if a == b:
                 company.remove(b)
-
+    print(amount)
 
 #Prints how much profit is made
 print('Profit made: $' + str(round(profit,2)))
@@ -60,5 +61,5 @@ print('Profit made: $' + str(round(profit,2)))
 print('Remaining: ' + str(x))
 
 #Prints to which company has the material been given to
-print('Companies that the material should be sold to: ' + str((sorted(company_sold_to))))
+print('Companies that the material should be sold to: ' + str(company_sold_to))
 
